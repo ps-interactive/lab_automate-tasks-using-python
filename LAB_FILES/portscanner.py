9878956930c55cf3154241d7a3697ccd36ffbf58
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2022-10-13 22:18:24 +0800

    Update portscanner.py

--- LAB_FILES/portscanner.py
+++ LAB_FILES/portscanner.py
@@ -3,9 +3,9 @@ import subprocess
 
 nm = nmap.PortScanner()
 print('Scanning, please wait...')
-#nm.scan('127.0.0.1','9090')
+nm.scan('127.0.0.1','9090')
 
 if 'Werkzeug httpd' in nm['127.0.0.1']['tcp'][9090]['product']:
   print('Identified target service, attempt to auto guess the password..')
-  #p = subprocess.run(['hydra', '-l', '', '-P','dictionary.txt','-s','9090', '-w' , '4',   '-f', '127.0.0.1','http-get', '/'  ], stdout=subprocess.PIPE)
+  # Add custom action code here
   print(p.stdout)
